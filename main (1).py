69d7b613790a291f1e9b2712beab21377684bd7a
Saravanan2092005 <144423081+Saravanan2092005@users.noreply.github.com>
2023-10-21 14:04:02 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,13 +1,30 @@
-def recur_factorial(n):  
-   if n == 1:  
-       return n  
-   else:  
-       return n*recur_factorial(n-1)    
-num = int(input("Enter a number: "))   
-if num < 0:  
-   print("Sorry, factorial does not exist for negative numbers")  
-elif num == 0:  
-   print("The factorial of 0 is 1")  
-else:  
-   print("The factorial of",num,"is",recur_factorial(num)) 
+class BankAccount():
+  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
+   self.__account_number=account_number  
+   self.__account_holder_name=account_holder_name
+   self.__account_balance=initial_balance
+  def deposit(self,amount):
+    if amount>0:
+      self.__account_balance+=amount
+      print("Deposit ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+  def withdraw(self,amount):
+    if amount>0 and amount<=self.__account_balance:
+      self.__account_balance-=amount
+      print("Withdraw ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+  def display_balance(self):
+    print ("Account balance for {} (Account #{}): ₹{}".format(
+        self.__account_holder_name,self.__account_number,
+      self.__account_balance))
 
+account=BankAccount(account_number="9344868020",
+                    account_holder_name="Amith Babu",
+                    initial_balance=50000.0)
+
+account.display_balance()
+account.deposit(1000.0)
+account.withdraw(500.0)
+account.display_balance()
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-#implemwnt a python program that determines whether the given year is a leap year or not using if elif statment
-year=int(input("enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-  print("the year is a leap year!")
-else:
-  print("the leap year isn't a leap year!")
+class Player:
+  def Play(self):
+      print("The Player is Playing cricket.")
+    
+class Batsman(Player):
+    def Play(self):
+        print("The batsman is       batting.")
+        
+class Bowler(Player):
+  
+    def Play(self):
+        print("The bowler is bowling.")
+                 
+batsman = Batsman()
+bowler = Bowler()
+
+
+batsman.Play()
+bowler.Play()
